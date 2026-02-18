8314d240744b8ee6f0b0321404b6536944446f72
get-unknown-err0r <getunknownerror@gmail.com>
2026-02-18 22:34:33 +0530

    Skills fix

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,4 @@
-import "./Skills.css";
+import "./Skills.css"
 import {
   FaReact,
   FaNodeJs,
@@ -7,32 +7,50 @@ import {
   FaHtml5,
   FaCss3Alt,
   FaJsSquare,
+  FaRoute,
+  FaKey,
 } from "react-icons/fa";
-import { SiMongodb, SiExpress } from "react-icons/si";
-import { SiRedux, SiTailwindcss, SiVercel } from "react-icons/si";
-import { FaRoute, FaKey } from "react-icons/fa";
-import { SiPostman } from "react-icons/si";
+
+import {
+  SiMongodb,
+  SiExpress,
+  SiRedux,
+  SiTailwindcss,
+  SiVercel,
+  SiPostman,
+  SiTypescript,
+  SiAxios,
+  SiNginx,
+  SiRender
+} from "react-icons/si";
+
+import { TbBrandFramerMotion } from "react-icons/tb";
 
 const skills = [
   { icon: <FaReact />, name: "React.js", level: "Advanced" },
   { icon: <FaRoute />, name: "React Router", level: "Expert" },
   { icon: <SiRedux />, name: "Redux Toolkit", level: "Advanced" },
   { icon: <FaJsSquare />, name: "JavaScript", level: "Advanced" },
+  { icon: <SiTypescript />, name: "TypeScript", level: "Intermediate" },
+
   { icon: <FaHtml5 />, name: "HTML5", level: "Expert" },
   { icon: <FaCss3Alt />, name: "CSS3", level: "Expert" },
   { icon: <SiTailwindcss />, name: "Tailwind CSS", level: "Expert" },
+  { icon: <TbBrandFramerMotion />, name: "Framer Motion", level: "Intermediate" },
 
   { icon: <FaNodeJs />, name: "Node.js", level: "Advanced" },
   { icon: <SiExpress />, name: "Express.js", level: "Advanced" },
   { icon: <FaDatabase />, name: "REST APIs", level: "Pro" },
   { icon: <FaKey />, name: "JWT Auth", level: "Advanced" },
+  { icon: <SiAxios />, name: "Axios", level: "Advanced" },
 
   { icon: <SiMongodb />, name: "MongoDB", level: "Advanced" },
-  { icon: <SiPostman />, name: "Postman ", level: "Advanced" },
+  { icon: <SiPostman />, name: "Postman", level: "Advanced" },
 
   { icon: <FaGitAlt />, name: "Git & GitHub", level: "Pro" },
   { icon: <SiVercel />, name: "Vercel", level: "Advanced" },
-
+  { icon: <SiRender />, name: "Render", level: "Advanced" },
+  { icon: <SiNginx />, name: "Nginx", level: "Basic" },
 ];
 
 const Skills = () => {
